23c90dfbf27e8d442fa29de3c94153670d8d240e
VikramBombhi <vbombhi@gmail.com>
2016-02-13 19:30:49 -0500

    changed file name

--- AI.cpp
+++ AI.cpp
@@ -3,81 +3,81 @@
 
 using namespace std;
 
-struct pos_t{
+struct pos {
     int x;
     int y;
     int prob;
 };
 
-vector<pos_t> bteam;
-vector<pos_t> rteam;
+vector<pos> blueTeam;
+vector<pos> redTeam;
 
 void init_assignment(){
     //----------------------------BLUE TEAM----------------------------\\
     //Push back new chip created with default constructor.
-    bteam.push_back(pos_t());
+    blueTeam.push_back(pos());
     //Vector now has 1 element @ index 0
-    bteam[0].x = 1;
-    bteam[0].y = 0;
+    blueTeam[0].x = 1;
+    blueTeam[0].y = 0;
 
     //Push back new chip created with default constructor.
-    bteam.push_back(pos_t());
+    blueTeam.push_back(pos());
     //Vector now has 1 element @ index 1
-    bteam[1].x = 3;
-    bteam[1].y = 0;
+    blueTeam[1].x = 3;
+    blueTeam[1].y = 0;
 
     //Push back new chip created with default constructor.
-    bteam.push_back(pos_t());
+    blueTeam.push_back(pos());
     //Vector now has 1 element @ index 2
-    bteam[2].x = 2;
-    bteam[2].y = 1;
+    blueTeam[2].x = 2;
+    blueTeam[2].y = 1;
 
     //Push back new chip created with default constructor.
-    bteam.push_back(pos_t());
+    blueTeam.push_back(pos());
     //Vector now has 1 element @ index 3
-    bteam[3].x = 1;
-    bteam[3].y = 2;
+    blueTeam[3].x = 1;
+    blueTeam[3].y = 2;
 
     //-----------------------------RED TEAM----------------------------\\
     //Push back new chip created with default constructor.
-    rteam.push_back(pos_t());
+    redTeam.push_back(pos());
     //Vector now has 1 element @ index 0
-    rteam[0].x = 0;
-    rteam[0].y = 4;
+    redTeam[0].x = 0;
+    redTeam[0].y = 4;
 
     //Push back new chip created with default constructor.
-    rteam.push_back(pos_t());
+    redTeam.push_back(pos());
     //Vector now has 1 element @ index 1
-    rteam[1].x = 2;
-    rteam[1].y = 4;
+    redTeam[1].x = 2;
+    redTeam[1].y = 4;
 
     //Push back new chip created with default constructor.
-    rteam.push_back(pos_t());
+    redTeam.push_back(pos());
     //Vector now has 1 element @ index 2
-    rteam[2].x = 2;
-    rteam[2].y = 3;
+    redTeam[2].x = 2;
+    redTeam[2].y = 3;
 
     //Push back new chip created with default constructor.
-    rteam.push_back(pos_t());
+    redTeam.push_back(pos());
     //Vector now has 1 element @ index 3
-    rteam[3].x = 3;
-    rteam[3].y = 3;
+    redTeam[3].x = 3;
+    redTeam[3].y = 3;
 }
 
 void threat_check(int b){
         for (int j = 0; j < 4; j++) {
             //Check bottom left
-            if ((bteam[b].x == rteam[j].x + 1) && (bteam[b].y == rteam[j].y - 1)) {
+            if ((blueTeam[b].x == redTeam[j].x + 1) && (blueTeam[b].y == redTeam[j].y - 1)) {
                 cout << "left";
                 cout << endl;
-                cout << "original x,y coordinates:" << bteam[b].x << bteam[b].y;
+                cout << "original x,y coordinates:" << blueTeam[b].x << blueTeam[b].y;
                 cout << endl;
             }
             //Check bottom right
-            if ((bteam[b].x == rteam[j].x - 1) && (bteam[b].y == rteam[j].y - 1)) {
+            if ((blueTeam[b].x == redTeam[j].x - 1) && (blueTeam[b].y == redTeam[j].y - 1)) {
                 cout << "right";
                 cout << endl;
-                cout << "original x,y coordinates:" << bteam[b].x << bteam[b].y;
+                cout << "original x,y coordinates:" << blueTeam[b].x << blueTeam[b].y;
                 cout << endl;
             }
         }
@@ -89,7 +89,7 @@ void move_check(){
     }
 }
 
-int main() {
+int main(){
     init_assignment();
     move_check();
 }
