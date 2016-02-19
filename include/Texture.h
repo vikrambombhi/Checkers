02a61f36ea03c52da169c537b31651270807bf2b
Jacky Chiu <jacky.chiu888@gmail.com>
2016-02-19 01:01:53 -0500

    Minor change

--- include/Texture.h
+++ include/Texture.h
@@ -12,7 +12,7 @@
 #include <string>
 #ifdef _WIN32
     #include <SDL.h>
-    #include<SDL_image.h>
+    #include <SDL_image.h>
 #elif __APPLE__
     #include <SDL2/SDL.h>
     #include <SDL2_image/SDL_image.h>
