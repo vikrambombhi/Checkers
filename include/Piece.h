33e65bebb12b215a7604f64b32f4a0992aa429e4
Jacky Chiu <jacky.chiu888@gmail.com>
2016-05-02 21:53:43 -0400

    Add new method of finding best direction a piece should move

--- include/Piece.h
+++ include/Piece.h
@@ -9,7 +9,9 @@
 #ifndef Piece_h
 #define Piece_h
 
+#include <vector>
 
+using namespace std;
 
 class Piece{
 public:
@@ -20,7 +22,9 @@ public:
     int x;
     int y;
     int potential;
+    int directionValues[4] = {-99999999, -99999999, -99999999, -99999999};
     int bestDirection;
+    void findBestDirection();
 private:
     bool king;
 };
--- src/Piece.cpp
+++ src/Piece.cpp
@@ -7,6 +7,9 @@
 //
 
 #include "../include/Piece.h"
+#include <cstdlib>
+#include <stdlib.h>
+#include <time.h>
 
 Piece::Piece() {
     x = 0;
@@ -26,4 +29,26 @@ bool Piece::isKing() {
 
 void Piece::makeKing() {
     king = true;
+}
+
+void Piece::findBestDirection(){
+    
+    int largest = directionValues[0];
+    vector<int> largestVector;
+    
+    for (int i=1; i<4; i++) {
+        if (largest < directionValues[i]) {
+            largest = directionValues[i];
+        }
+    }
+    
+    for(int j=0; j<4; j++){
+        if (largest <= directionValues[j]) {
+            largestVector.push_back(directionValues[j]);
+        }
+    }
+    
+    srand(static_cast<unsigned int>(time(NULL)));
+    bestDirection = rand() % largestVector.size();
+    potential = largest;
 }
